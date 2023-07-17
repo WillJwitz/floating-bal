e51730ce4d17d95791bd99d9afd2d6a54a177830
EnzoR9 <enzorussillo44@gmail.com>
2023-07-16 22:18:41 -0400

    basic pickup prototype

--- floating bal/Assets/MouseInter.cs
+++ floating bal/Assets/MouseInter.cs
@@ -7,8 +7,19 @@ public class MouseInter : MonoBehaviour
 {
     [SerializeField]
     private InputAction mouseClick;
+    [SerializeField]
+    private float mouseDragPhysSpeed = 10;
+    [SerializeField]
+    private float mouseDragSpeed = .1f;
 
     private Camera mainCamera;
+    private Vector3 velocity = Vector3.zero;
+    private WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnEnable()
     {
@@ -25,5 +36,32 @@ public class MouseInter : MonoBehaviour
     private void MousePressed(InputAction.CallbackContext context)
     {
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray);
+            if (hit2D.collider != null && (hit2D.collider.gameObject.CompareTag("Draggable")))
+            {
+                StartCoroutine(dragUpdate(hit2D.collider.gameObject));
+            }
+
+    }
+
+    private IEnumerator dragUpdate(GameObject clickedObject)
+    {
+        float initialDistance = Vector3.Distance(clickedObject.transform.position, mainCamera.transform.position);
+        clickedObject.TryGetComponent<Rigidbody2D>(out var rb);
+        while (mouseClick.ReadValue<float>() != 0)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (rb != null)
+            {
+                Vector3 direction = ray.GetPoint(initialDistance) - clickedObject.transform.position;
+                rb.velocity = direction * mouseDragPhysSpeed;
+                yield return waitForFixedUpdate;
+            }
+            else
+            {
+                clickedObject.transform.position = Vector3.SmoothDamp(clickedObject.transform.position, ray.GetPoint(initialDistance), ref velocity, mouseDragPhysSpeed);
+                yield return null;
+            }
+        }
     }
 }
