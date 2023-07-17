bbb667ebe9db0b6e6fd9090c2acf2a90e879e52b
WillJwitz <william.jurewitz12@gmail.com>
2023-07-16 22:56:02 -0400

    Camera Follow Polished

--- floating bal/Assets/Script/CamFollow.cs
+++ floating bal/Assets/Script/CamFollow.cs
@@ -10,26 +10,44 @@ public class CamFollow : MonoBehaviour
     public GameObject Player;
     public GameObject Camera;
 
-    public float distance; // 2.5
+    public float distanceSet; // 2.5
     public float step; // 4
+    float stepDef;
+    public float multiplier;
 
     bool centerLock = false;
+    private void Start()
+    {
+        stepDef = step;
+    }
     void FixedUpdate()
     {
         CamPos = transform.position;
         PlayerPos = Player.transform.position;
-        if(centerLock == false && Mathf.Abs((CamPos-PlayerPos).magnitude) > distance )
+        float distance = Mathf.Abs((CamPos - PlayerPos).magnitude);
+
+        if (centerLock == false && distance > distanceSet )
         {
             centerLock = true;
         }
         if(centerLock == true)
         {
             transform.position = Vector2.MoveTowards(transform.position, PlayerPos, step * Time.deltaTime);
-            if(CamPos == PlayerPos)
+            if(distance < 0.25)
             {
+                transform.position = PlayerPos;
                 centerLock = false;
+                if(step > stepDef)
+                {
+                    step -= multiplier;
+                } 
             }
         }
+        if(distance > distanceSet *2 && step == stepDef)
+        {
+            step += multiplier;
+        }
+
         transform.position = new Vector3(transform.position.x, transform.position.y, -10);
     }
 }
